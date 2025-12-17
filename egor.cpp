6569c2cc52db1a0c9eff02ee1d87f73499db28b7
Regrok15 <egorrokan@gmail.com>
2025-12-17 03:10:37 +0200

    Update egor.cpp

--- egor.cpp
+++ egor.cpp
@@ -1,1 +1,11 @@
 #include "funcs.h"
+#include <queue>
+#include <vector>
+
+struct CarPriceComparator {
+    bool operator()(const Car& a, const Car& b) const {
+        return a.pricePerDay > b.pricePerDay;
+    }
+};
+
+priority_queue<Car, vector<Car>, CarPriceComparator> carQueue;
