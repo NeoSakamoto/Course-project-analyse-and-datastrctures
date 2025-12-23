47a4ec711c7092e6ceeaa7eb96acaa7a7091e0ac
Regrok15 <egorrokan@gmail.com>
2025-12-23 21:32:33 +0200

    Update egor.cpp

--- egor.cpp
+++ egor.cpp
@@ -1,39 +1,74 @@
 #include "funcs.h"
-#include <queue>
 #include <vector>
-#include <chrono>
 #include <iostream>
 
 using namespace std;
 
-struct CarPriceComparator {
-    bool operator()(const Car& a, const Car& b) const {
-        return a.price > b.price;
+class BinaryHeap {
+private:
+    vector<Car> heap;
+
+    int parent(int i) { return (i - 1) / 2; }
+    int left(int i) { return 2 * i + 1; }
+    int right(int i) { return 2 * i + 2; }
+
+    void heapifyUp(int i) {
+        while (i > 0 && heap[parent(i)].price > heap[i].price) {
+            swap(heap[i], heap[parent(i)]);
+            i = parent(i);
+        }
+    }
+
+    void heapifyDown(int i) {
+        int smallest = i;
+        int l = left(i);
+        int r = right(i);
+
+        if (l < heap.size() && heap[l].price < heap[smallest].price)
+            smallest = l;
+
+        if (r < heap.size() && heap[r].price < heap[smallest].price)
+            smallest = r;
+
+        if (smallest != i) {
+            swap(heap[i], heap[smallest]);
+            heapifyDown(smallest);
+        }
+    }
+
+public:
+    void insert(const Car& c) {
+        heap.push_back(c);
+        heapifyUp(heap.size() - 1);
+    }
+
+    Car getMin() {
+        return heap[0];
+    }
+
+    void removeMin() {
+        heap[0] = heap.back();
+        heap.pop_back();
+        heapifyDown(0);
+    }
+
+    bool empty() {
+        return heap.empty();
     }
 };
 
-priority_queue<Car, vector<Car>, CarPriceComparator> carQueue;
-
 void egorTask() {
-    auto startInsert = chrono::high_resolution_clock::now();
+    BinaryHeap heap;
 
-    for (int i = 0; i < 1000; i++) {
+    for (int i = 0; i < 10; i++) {
         Car c;
         c.id = i;
         c.price = 500 + rand() % 500;
-        carQueue.push(c);
+        heap.insert(c);
     }
 
-    auto endInsert = chrono::high_resolution_clock::now();
-
-    auto startTop = chrono::high_resolution_clock::now();
-    Car cheapest = carQueue.top();
-    auto endTop = chrono::high_resolution_clock::now();
-
-    chrono::duration<double, milli> insertTime = endInsert - startInsert;
-    chrono::duration<double, nano> topTime = endTop - startTop;
-
-    cout << "Priority Queue (Heap):\n";
-    cout << "Insert time (1000 elements): " << insertTime.count() << " ms\n";
-    cout << "Get min price time: " << topTime.count() << " ns\n";
+    if (!heap.empty()) {
+        Car cheapest = heap.getMin();
+        cout << "Cheapest car price: " << cheapest.price << endl;
+    }
 }
